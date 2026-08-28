27c970c7393eb52f97d906b14a430bcbfa1f7cb5
Zoe <88523932+ink-zoey@users.noreply.github.com>
2026-08-27 20:19:01 -0700

    init

---
+++ src/GlobalUsings.cs
@@ -0,0 +1,15 @@
+﻿global using Daybreak.ChatTags.V1;
+global using Daybreak.Contracts.V1;
+global using Daybreak.EarlyLoader.V1;
+global using Daybreak.Hooks.V1;
+global using Daybreak.Hooks;
+global using Daybreak.Mathematics.V1;
+global using Daybreak.Models.V1;
+global using Daybreak.MonoMod.V1;
+global using Daybreak.Networking.V1;
+global using Daybreak.Rendering.V1;
+global using Daybreak.Rendering.Buffers.V1;
+global using Daybreak.Rendering.Buffers.Entities.V1;
+global using Daybreak.Resources.V1;
+global using VariableIndex = Daybreak.MonoMod.V1.VariableIndex; // Mono.Cecil.Cil.VariableIndex
+global using Player = Terraria.Player; // Terraria.ModLoader.BackupIO+Player
