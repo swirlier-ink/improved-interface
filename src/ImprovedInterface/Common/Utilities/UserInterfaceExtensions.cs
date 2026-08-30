59e90a990d3a9151df12bc3d355a223b4f8b28ff
Zoe <88523932+ink-zoey@users.noreply.github.com>
2026-08-29 19:38:04 -0700

    pause menu stub

---
+++ src/ImprovedInterface/Common/Utilities/UserInterfaceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Terraria.UI;
+
+namespace ImprovedInterface.Common;
+
+public static class UserInterfaceExtensions
+{
+    extension(UserInterface @interface)
+    {
+        public UIState? State
+        {
+            get => @interface.CurrentState;
+
+            set
+            {
+                if (@interface.State != value)
+                {
+                    @interface.SetState(value);
+                }
+            }
+        }
+    }
+}
--- src/ImprovedInterface/Content/PauseMenu/PauseMenuReplacement.cs
+++ src/ImprovedInterface/Content/PauseMenu/PauseMenuReplacement.cs
@@ -1,9 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ImprovedInterface.Common;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameInput;
+using Terraria.UI;
 
 namespace ImprovedInterface.Content.PauseMenu;
 
-internal class PauseMenuReplacement
+public static class PauseMenuReplacement
 {
+    private static UserInterface pauseInterface = new UserInterface();
+
+    [ModSystemHooks.UpdateUI]
+    private static void UpdateUI(GameTime gameTime)
+    {
+        if (!Main.ingameOptionsWindow)
+        {
+            return;
+        }
+
+        pauseInterface.Update(gameTime);
+    }
+
+    [GameInterfaceLayers.Replace(GameInterfaceLayers.IN_GAME_OPTIONS, InterfaceScaleType.UI, Name = $"{nameof(ImprovedInterface)}: Pause Menu")]
+    private static bool DrawPauseMenu()
+    {
+        if (!Main.ingameOptionsWindow)
+        {
+            pauseInterface.State = null;
+
+            return true;
+        }
+
+        // pauseInterface.State = ;
+
+        var sb = Main.spriteBatch;
+
+        sb.End(out var ss);
+        sb.Begin(in ss);
+        {
+            Main.instance.DrawInterface_16_MapOrMinimap();
+        }
+
+        // DrawInterface_16_MapOrMinimap runs PlayerInput::SetZoom_Unscaled
+        PlayerInput.SetZoom_UI();
+
+        sb.Restart(in ss);
+        {
+            pauseInterface.Draw(sb, Main.instance.gameTime);
+
+            Main.instance.DrawMouseOver();
+        }
+        sb.Restart(ss with { SamplerState = Main.SamplerStateForCursor });
+        {
+            Main.DrawCursor(Main.DrawThickCursor());
+
+            Main.instance.DrawInterface_40_InteractItemIcon();
+        }
+
+        // Return false as the pause menu stops all further layers from rendering
+        return false;
+    }
 }
